c72707ce35543256d3b6a3b44bcd520af5742ae5
y0onzi <68225723+y0onzi@users.noreply.github.com>
2022-07-07 21:44:28 +0900

    recover words

---
+++ Section1/7.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	char a[100], b[100];
+	int i, p=0;
+	gets(a);
+	for(i=0; a[i]!='\0';i++){
+		if(a[i]!=' '){
+			if(a[i]>=65 && a[i]<=90){
+				b[p++]=a[i]+32;
+			}
+			else
+				b[p++]=a[i];
+		}
+	}
+	b[p]='\0'; //p++ 
+	printf("%s\n", b);
+	
+	return 0;
+}
