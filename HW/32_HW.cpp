fdd192006d3fb24f60ff65a45cbe8b8e989de054
Hee-won <45660025+Hee-won@users.noreply.github.com>
2022-07-25 12:22:52 +0900

    Create 32_HW.cpp

---
+++ HW/32_HW.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <vector> 
+#include<stdlib.h>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int i, j, n,num,min;
+    scanf("%d", &n);
+    int arr[100];
+   
+        for (i =0; i<n; i++) {
+            scanf("%d", &arr[i]); //for 문 없이도 받을 수 있는지 확인
+        }
+        for (i = 0; i < n; i++) {
+            min = i;
+            for (j = i+1; i < n; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+                num = arr[i];
+                arr[i] = arr[min];
+                arr[min] = num;
+            }
+        }
+        for (i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+   
+    return 0;
+}
+
+//32
