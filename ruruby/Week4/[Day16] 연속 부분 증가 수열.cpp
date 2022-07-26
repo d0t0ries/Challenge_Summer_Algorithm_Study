ce8cce0c30ab05b4854ebf7554f3034283288276
ruruby <63354563+ruruby@users.noreply.github.com>
2022-07-26 23:58:04 +0900

    Add files via upload

---
+++ ruruby/Week4/[Day16] 연속 부분 증가 수열.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	int cnt = 1;
+	int max_cnt = 0;
+
+	do {
+		cin >> n;
+	} while (n < 5 or n>100000);
+
+	int *ptr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> ptr[i];
+		if (ptr[i] > 100000)//입력받은 숫자가 100,000이하가 아닐 경우, i를 1줄임으로써 반복문 다시 돌아서 해당 인덱스에 값 다시 받음
+			i--;
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		if (ptr[i] <= ptr[i + 1]) {
+			cnt += 1;
+			if (cnt > max_cnt) {
+				max_cnt = cnt;//max_cnt와 현재 cnt 변수의 값과 비교해서 cnt가 더 크면 max_cnt에 cnt 값을 저장
+			}
+		}
+		else {
+			cnt = 1;
+		}
+	}
+
+	cout << max_cnt;
+	delete[] ptr;
+	return 0;
+}
---
+++ ruruby/Week4/[Day17] Jolly Jumpers.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N;
+	do {
+		cin >>  N;
+	} while (N < 3 or N>100);
+
+	int *arrayN = new int[N];
+	int *distanceofarrayN = new int[N - 1];//배열의 두 값의 차이를 담을 배열 
+
+	for (int i = 0; i < N; i++) {//사용자가 입력하는 수열의 값 N개 저장 
+		cin >> arrayN[i];
+	}
+
+	for (int i = 0; i < N - 1; i++) {
+		distanceofarrayN[i] = abs(arrayN[i] - arrayN[i + 1]);
+	}
+
+	sort(distanceofarrayN, distanceofarrayN + (N - 1));//distanceofarrayN[N-2]까지 오름차순 정렬
+
+	for (int i = 0; i < N - 1; i++) {
+		if (distanceofarrayN[i] != i+1) {
+			cout << "NO" << endl;
+			delete[] arrayN;
+			delete[] distanceofarrayN;
+			return 0;
+		}
+	}
+	cout << "YES" << endl;
+
+
+	delete[] arrayN;
+	delete[] distanceofarrayN;
+
+	return 0;
+}
