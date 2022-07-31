b62edcfa863ce09369a4938557d30d7f3e247c02
Hee-won <45660025+Hee-won@users.noreply.github.com>
2022-07-31 22:26:49 +0900

    Create 38_HW.cpp

---
+++ HW/38_HW.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<vector> 
+#include<stdlib.h>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int i, j, n, tmp = 0, pos, cnt = 0;
+    scanf("%d", &n);
+    vector<int> is(n + 1), os(n + 1);
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &is[i]);
+    }
+    for (i = n; i >=1; i--) {
+        pos = i;
+        for (j = 1; j <= is[i]; j++) {
+            os[pos] = os[pos + 1];
+            pos++;
+        }
+        os[pos] = i;
+    }
+
+                for (i = 1; i <= n; i++) {
+                    printf("%d ",os[i]);
+    }
+    return 0;
+}
+//38
